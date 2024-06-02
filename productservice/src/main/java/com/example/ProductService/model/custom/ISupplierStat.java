0683301904611fcff80c48eaa38667f9734ead05
Nguyen Phuc Son <pson141002@gmail.com>
2024-06-02 15:48:34 +0700

    make SupplierStat extends from Supplier for cleaner code

--- productservice/src/main/java/com/example/ProductService/model/custom/ISupplierStat.java
+++
@@ -1,8 +0,0 @@
-package com.example.ProductService.model.custom;
-
-public interface ISupplierStat {
-    Integer getId();
-    String getName();
-    String getDescription();
-    Integer getProductAmount();
-}
