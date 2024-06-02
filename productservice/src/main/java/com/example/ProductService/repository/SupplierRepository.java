74ab9ec4238e4226b167cbd7b93e8834044a8133
Nguyen Phuc Son <pson141002@gmail.com>
2024-06-02 15:51:34 +0700

    delete unecessary comment

--- productservice/src/main/java/com/example/ProductService/repository/SupplierRepository.java
+++ productservice/src/main/java/com/example/ProductService/repository/SupplierRepository.java
@@ -10,17 +10,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
-//    @Query(value = """
-//        SELECT
-//        	s.id, s.name, s.description, SUM(ip.quantity) as productAmount
-//        FROM supplier AS s
-//        	INNER JOIN import_order AS io ON io.supplier_id = s.id
-//            INNER JOIN import_product AS ip ON ip.import_order_id = io.id
-//        WHERE io.import_date BETWEEN :startDate AND :endDate
-//        GROUP BY s.id, s.name, s.description
-//        ORDER BY productAmount DESC
-//        """, nativeQuery = true)
-//    List<ISupplierStat> getSupplierStatByProductAmountList(@Param("startDate") String startDate, @Param("endDate") String endDate);
     @Query(value = """
         SELECT
         	s.id, s.name, s.description, SUM(ip.quantity) as product_amount
